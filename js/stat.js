64377011820af46a1530b9601d4b535e102d2bca
MamaedovArif <arifmho@yandex.ru>
2020-01-22 18:12:27 +0300

    hsl

--- js/stat.js
+++ js/stat.js
@@ -1,22 +1,55 @@
 'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var rando = Math.random().toFixed(2) * 100;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 25;
+var VERT_GAP = 250;
+var COLUMN = 40;
+var DIST = 50;
+var MAX_HEIGHT = 110;
+var barWidth = CLOUD_HEIGHT - GAP - FONT_GAP;
+var height = 150;
+
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+var getMaxElement = function(arr) {
+  if (arr[0] === undefined) {
+    return undefined;
+  }
+  var maxElement = arr[0];
+  for (var i of arr) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000000'
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов: ', 120, 60);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = "#000000";
+    ctx.fillText(players[i], CLOUD_X + GAP + FONT_GAP + i * (COLUMN + DIST), CLOUD_HEIGHT - GAP);
+    ctx.fillStyle = 'hsl(240, 100%, 50%)';
+    ctx.fillRect(CLOUD_X + GAP + FONT_GAP + i * (COLUMN + DIST), barWidth, COLUMN, -(height * times[i] / maxTime));
+  }
 }
 
-var rebderText = function(ctx, size, family, text, x, y) {
-  ctx.font = size + ' ' + family;
-  ctx.fillText(text, x, y)
-}
 
-window.renderStatistics = function(ctx, names, times) {
-  rebderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  rebderCloud(ctx, 100, 10,, 'white');
 
-  rebderText(ctx, '16px', 'PT Mono', 'Ура вы победили!', 0, 10);
-  rebderText(ctx, '16px', 'PT Mono', 'Список результатов: ', 0, 30);
-
-}
