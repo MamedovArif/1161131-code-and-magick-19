905d374c42c5f1065fa590a345485ac72f6ec2dc
MamaedovArif <arifmho@yandex.ru>
2020-01-25 15:03:06 +0300

    шлифуем второе задание

--- js/stat.js
+++ js/stat.js
@@ -3,20 +3,24 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
-var CLOUD_X = 100;
+var CLOUD_X = 100; // начальные координаты
 var CLOUD_Y = 10;
-var GAP = 10;
+var GAP = 10; // отступ тени от облака
 var HOR_GAP = 25;
 var VERT_GAP = 35;
-var COLUMN = 40;
-var DIST = 50;
-var coordY = CLOUD_HEIGHT - VERT_GAP;
-var height = 150;
+var COLUMN = 40; // ширина столбца
+var DIST = 50; // расстояние между столбцами
+var coordY = CLOUD_HEIGHT - VERT_GAP; //
+var height = 150; // максимальная высота
+
+var distX1 = CLOUD_X + GAP + HOR_GAP;
+var distX2 = COLUMN + DIST;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 1; i < arr.length; i++) {
@@ -25,6 +29,11 @@ var getMaxElement = function (arr) {
     }
   }
   return maxElement;
+};
+
+var randomColor = function (hslValue) {
+  var random = Math.random().toFixed(2) * 100;
+  return 'hsl(' + hslValue + ',' + random + '%, 50%)';
 };
 
 window.renderStatistics = function (ctx, players, times) {
@@ -40,15 +49,14 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000000';
-    ctx.fillText(players[i], CLOUD_X + GAP + HOR_GAP + i * (COLUMN + DIST), CLOUD_HEIGHT - GAP);
+    ctx.fillText(players[i], distX1 + i * distX2, CLOUD_HEIGHT - GAP);
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'red';
     } else {
-      var rando = Math.random().toFixed(2) * 100;
-      ctx.fillStyle = 'hsl(240,' + rando + '%, 50%)';
+      ctx.fillStyle = randomColor(240);
     }
-    ctx.fillRect(CLOUD_X + GAP + HOR_GAP + i * (COLUMN + DIST), coordY, COLUMN, -(height * times[i] / maxTime));
+    ctx.fillRect(distX1 + i * distX2, coordY, COLUMN, -(height * times[i] / maxTime)); // чтобы столбец отсчитывался снизу вверх
     ctx.fillStyle = '#000000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + HOR_GAP + i * (COLUMN + DIST), CLOUD_HEIGHT - GAP - VERT_GAP - (height * times[i] / maxTime));
+    ctx.fillText(Math.round(times[i]), distX1 + i * distX2, CLOUD_HEIGHT - GAP - VERT_GAP - (height * times[i] / maxTime));
   }
 };
