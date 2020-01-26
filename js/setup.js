69c89730a378a52c39620c7492e1780962a53ad0
MamaedovArif <arifmho@yandex.ru>
2020-01-26 13:47:35 +0300

    fragment

--- js/setup.js
+++ js/setup.js
@@ -7,22 +7,36 @@ function randomInteger(min, max) {
 var catalog = document.querySelector('.setup');
 catalog.classList.remove('hidden');
 
-var persons = [];
+var wizards = [];
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 for (var i = 0; i < 4; i++) {
-  var fullname = [`${names[randomInteger(0, names.length - 1)]}`, `${surnames[randomInteger(0, surnames.length - 1)]}`];
   var mainObject = {
     number: i,
-    name: `${fullname[randomInteger(0, 1)]} ${fullname[randomInteger(0, 1)]}`,
+    name: `${names[randomInteger(0, names.length - 1)]} ${surnames[randomInteger(0, surnames.length - 1)]}`,
     coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
     eyesColor: eyesColors[randomInteger(0, eyesColors.length - 1)],
   }
 
-  persons.push(mainObject);
+  wizards.push(mainObject);
 }
 
-console.log(persons);
+console.log(wizards);
+
+var similarListElement = catalog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+for (var j = 0; j < wizards.length; j++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[j].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[j].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[j].eyesColor;
+
+  similarListElement.appendChild(wizardElement);
+};
+console.log(similarListElement);
