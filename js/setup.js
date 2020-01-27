c8ae79882e11cea065a196089adcc2d29230dee7
MamaedovArif <arifmho@yandex.ru>
2020-01-27 12:09:19 +0300

    let

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function randomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+  var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
